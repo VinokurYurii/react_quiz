18d62b8dcc92575c31651f301e193f5417807a68
Vynokur Yurii <vynokur.yurii@gmail.com>
2024-03-23 10:52:10 +0200

    Add start functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import {useEffect, useReducer} from "react";
+import error from "./Error";
+
+
+const initialState = {
+  questions: [],
+  status: "loading", // loading, error, ready, active, finished
+}
+function reducer(state, action) {
+  switch (action.type) {
+    case "addQuestions":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready"
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      }
+    default:
+      throw new Error("Actions unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then(data => dispatch({type: "addQuestions", payload: data}))
+      .catch(err => dispatch({type: "dataFailed"}));
+  }, []);
+
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
