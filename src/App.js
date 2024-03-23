4cb97e377aab75c94dbeda5c5153289781de1fbf
Vynokur Yurii <vynokur.yurii@gmail.com>
2024-03-23 22:15:51 +0200

    Next button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import {StartScreen} from "./components/StartScreen";
 import {Question} from "./components/Question";
+import {NextButton} from "./components/NextButton";
 
 
 const initialState = {
@@ -40,7 +41,13 @@ function reducer(state, action) {
         points: action.payload === question.correctOption ?
           state.points + question.points :
           state.points
-      }
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null
+      };
     default:
       throw new Error("Actions unknown");
   }
@@ -73,11 +80,14 @@ function App() {
         }
         {
           status === 'active' &&
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer}/>
+          </>
         }
       </Main>
     </div>
